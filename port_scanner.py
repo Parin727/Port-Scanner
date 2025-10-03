02861e63e5be6c3b301d171f31b3d81d9afd1b73
Parin727 <parinarora2007@gmail.com>
2025-10-04 01:21:42 +0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,3 +2,10 @@
 import sys 
 import socket
 from datetime import datetime
+
+#Define our target
+if len(sys.argv)==2: #argv is the amount of arguements we are giving
+    target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+    print("Invalid amount of arguements.")
+    print("Syntax: python3 port_scanner.py <IP>")
